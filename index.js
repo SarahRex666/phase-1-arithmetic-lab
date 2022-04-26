681484fe3ef735d4dc14370ff2bd49dbc41cf1a4
SarahRex <sarahrexmakes@gmail.com>
2022-04-25 20:13:18 -0400

    commit tree 1408233de675a54f02957b30a37b64ab8ef42034

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const num3 = 8;
+const num4 = 20;
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+multiply(num1, num2);
+
+function random() {
+  return math.random();
+}
+
+function mod(num3, num4) {
+  return num1 % num2;
+}
+
+function max() {
+  return math.max();
+}
